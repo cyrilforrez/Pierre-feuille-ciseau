b5da73d7ec1159b94d6cbddd0f01d17914fb3894
cyrilforrez <cyilforrez@gmail.com>
2017-06-28 14:13:21 +0200

    modif script js

--- script.js
+++ script.js
@@ -1,58 +1,49 @@
-//================================== Var definitions. ======================================
-var user = "";
-var computer = "";
-var nb;
-var won = 0, lost = 0;
+var choixUtilisateur;
+var choixOrdi ;
+var score = 0
+while (score < 4) {
+	choixUtilisateur = prompt("Choisissez-vous pierre, feuille, ou ciseaux ?");
+	choixOrdi = Math.random();
+
+if (choixOrdi < 0.34) {
+	choixOrdi = "pierre";
+	} else if(choixOrdi <= 0.67) {
+	choixOrdi = "feuille";
+} else {
+	choixOrdi = "ciseaux";
+}
 
 
-function compute()
-{
-	nb = Math.floor(Math.random()*3) + 1;
-	if (nb == 1)
-		computer = "Pierre";
-	else if (nb == 2)
-		computer = "Feuille";
-	else if (nb == 3)
-		computer = "Ciseau";
-}
-//=================================== Get user input. ======================================
-function res(form,val)
-{
-	if (user != "")
-		return;
-	form.texte.value = val;
-	user = val;
-}
-//================================= Test for validity. =====================================
-function testing(form)
-{
-	if (user == "" || computer == "")
-		return;
-	if (user == computer)
-	{
-		alert("Gagné !");
-		won += 1;
-		calcScore(form);
+
+	var comparer = function(choix1, choix2) {
+	    if (choix1 === choix2) {
+	        return "Egalité !";
+	    }
+	     else if (choix1 == "pierre") {
+	        if(choix2 == "ciseaux") {
+	            return "Vous avez gagné!";
+	        }
+	        else {
+	            return "Vous avez perdu!";
+	        }
 	}
-	else
-	{
-		alert("Perdu !\nL'ordinateur avait choisi " + computer + ".");
-		lost += 1;
-		calcScore(form);
+	    else if (choix1 =="feuille") {
+	        if (choix2 =="pierre") {
+	            return "Vous avez gagné!";
+	        }
+	        else {
+	            return "Vous avez perdu!";
+	        }
+	    }
+	    else if (choix1 =="ciseaux") {
+	        if (choix2 =="feuille") {
+	            return "Vous avez gagné!";
+	        }
+	        if (choix2 =="pierre") {
+	            return "Vous avez perdu!";
+	        }
+	    }
 	}
+	alert("Ordinateur: "+choixOrdi +"."+" "+ "Vous: "+choixUtilisateur +".");
+	alert(comparer(choixUtilisateur,choixOrdi));
 }
-//=================================== Sum up score. ========================================
-function calcScore(form)
-{
-	document.getElementById("score").innerHTML = "Gagné: " + won + " fois. Perdu: " + lost + " fois."
-}
-//====================================== Reset. ============================================
-function raz(form)
-{
-	form.texte.value = "";
-	user = "";
-	computer = "";
-}
-
-
-
